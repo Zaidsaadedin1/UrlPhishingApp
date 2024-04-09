1d91379e9c551504a16311ab0bfc4252d8cde138
zaidsaadedin1 <zaidsaadedin11@gmail.com>
2024-04-09 17:33:43 +0300

    add dount chart to show algos results

--- components/HomePage/HomePage.js
+++ components/HomePage/HomePage.js
@@ -7,42 +7,50 @@ import {
   Button,
   StyleSheet,
   SafeAreaView,
+  TouchableHighlight,
 } from "react-native";
-import DonutChart from "../../DonutChart/DonutChart";
+import { apis } from "../../Api/Apis";
+import { ProgressChart } from "react-native-chart-kit";
+import LoadingDots from "react-native-loading-dots";
 
 function HomePage() {
   const [url, setUrl] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([]);
   const [error, setError] = useState("");
-
+  const data = {
+    labels: ["Swim", "Bike", "Run"],
+    data: [0.0, 0.0, 0.0],
+  };
   async function detectPhishing() {
     if (!url) {
       return;
     }
     setIsLoading(true);
     setError("");
-
-    try {
-      // Simulating API call with setTimeout
-      setTimeout(() => {
+    setTimeout(async () => {
+      try {
+        const response = await apis.checkApi(url);
+        if (response.status === 200) {
+          setIsLoading(false);
+          setData([{ title: "Fake URL Detected", score: response.data.score }]);
+          setUrl("");
+        } else {
+          throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error("Error checking URL:", error);
         setIsLoading(false);
-        setData([{ title: "Fake URL Detected", score: 95 }]);
+        setError("Error checking URL. Please try again.");
         setUrl("");
-      }, 2000); // Simulate a 2-second delay
-    } catch (error) {
-      console.error("Error checking URL:", error);
-      setIsLoading(false);
-      setError("Error checking URL. Please try again.");
-      setUrl("");
-    }
+      }
+    }, 2000);
   }
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={styles.container}>
         <View style={styles.header}>
-          <Text style={styles.title}>Phishing Detector App</Text>
+          <Text style={styles.title}>Phishing Detector</Text>
         </View>
         <View style={styles.inputContainer}>
           <TextInput
@@ -52,29 +60,37 @@ function HomePage() {
             onChangeText={(text) => setUrl(text)}
           />
           <View style={styles.buttonContainer}>
-            <View style={styles.buttonView}>
-              <Button
-                color="red"
-                title="Check"
-                disabled={isLoading}
+            <View>
+              <TouchableHighlight
+                style={styles.submit}
                 onPress={() => detectPhishing()}
-                style={styles.button}
-              />
+                underlayColor="#fff"
+              >
+                <Text style={styles.submitText}>Check Url</Text>
+              </TouchableHighlight>
             </View>
           </View>
         </View>
         {error ? <Text style={styles.errorText}>{error}</Text> : null}
         <View style={styles.resultContainer}>
           {isLoading ? (
-            <Text style={styles.loadingText}>Checking...</Text>
+            <LoadingDots />
           ) : (
-            data.map((item, index) => (
-              <View key={index} style={styles.resultItem}>
-                <Text style={styles.resultTitle}>{item.title}</Text>
-                <Text style={styles.resultScore}>Score: {item.score}</Text>
-                <DonutChart data={item} />
-              </View>
-            ))
+            <View style={styles.resultItem}>
+              <ProgressChart
+                data={data}
+                width={300}
+                height={220}
+                strokeWidth={16}
+                radius={32}
+                hideLegend={false}
+                chartConfig={{
+                  color: (opacity = 1) => `rgba(26, 34, 146, ${opacity})`,
+                  backgroundGradientFrom: "#fff",
+                  backgroundGradientTo: "#fff",
+                }}
+              />
+            </View>
           )}
         </View>
       </ScrollView>
@@ -99,6 +115,8 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
+    textTransform: "uppercase",
+    color: "rgba(26, 34, 165, 0.8)",
   },
   inputContainer: {
     marginBottom: 20,
@@ -121,12 +139,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   resultItem: {
-    borderWidth: 1,
-    borderColor: "#ccc",
+    alignItems: "center",
     borderRadius: 5,
     paddingHorizontal: 10,
     paddingVertical: 8,
     marginBottom: 10,
+    backgroundColor: "white",
   },
   resultTitle: {
     fontSize: 18,
@@ -144,11 +162,24 @@ const styles = StyleSheet.create({
   },
   errorText: {
     textAlign: "center",
-    color: "red",
+    color: "white",
     marginBottom: 10,
   },
-  button: {
+  submit: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    width: 150,
+  },
+  submitText: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    color: "#fff",
+    textAlign: "center",
+    backgroundColor: "rgba(26, 34, 165, 0.8)",
     borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
   },
 });
 
